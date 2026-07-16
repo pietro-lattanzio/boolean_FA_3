7c04a44b1d1ede3eb708b01891e61bd6c1ffa082
Pietro Lattanzio <pietrolattanzio@Mac.station>
2026-07-16 17:36:49 +0200

    added n 10

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array_printer
 
 if __name__ == "__main__":
-    array = [1, 2, 3, 4, 5]
-    array_printer.print_array(array)
+    array = [1, 2, 3, 4, 5,6,7,8,9,10]
+    array_printer.print_array_error_even(array)
